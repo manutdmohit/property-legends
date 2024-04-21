533b65798e4f9bafffeba0d84b1d53226a95fb32
Mohit Saud <mohit@prabidhilabs.com>
2024-04-21 18:10:51 +0545

    Create layout homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'Property Legends | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;

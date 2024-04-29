a169c23bfd57a9ad49fcf7f549762c6b76efe58b
Mohit Saud <mohit@prabidhilabs.com>
2024-04-29 15:59:40 +0545

    Next- Auth, Sessions and Google Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 
 import '../assets/styles/globals.css';
-import Footer from '../components/Footer';
 
 export const metadata = {
   title: 'Property Legends | Find The Perfect Rental',
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 

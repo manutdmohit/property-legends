c7a922d1ffbb986062c2562b8efa9058912d941e
Mohit Saud <mohit@prabidhilabs.com>
2024-05-22 11:44:36 +0545

    Update spelling of dynamic

--- app/api/messages/unread-count/route.js
+++ app/api/messages/unread-count/route.js
@@ -2,7 +2,7 @@ import connectDB from '@/config/database';
 import Message from '@/models/Message';
 import { getSessionUser } from '@/utils/getSessionUser';
 
-export const dymanic = 'force-dymanic';
+export const dynamic = 'force-dymanic';
 
 // GET /api/messages/unread-count
 export const GET = async (request) => {

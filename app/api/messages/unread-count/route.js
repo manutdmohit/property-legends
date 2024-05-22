c1bc764bc98bd3ce2680539a9ad3e77a38ec8764
Mohit Saud <mohit@prabidhilabs.com>
2024-05-22 12:12:59 +0545

    Add:: export const dynamic = 'force-dynamic';

--- app/api/messages/unread-count/route.js
+++ app/api/messages/unread-count/route.js
@@ -1,8 +1,8 @@
+export const dynamic = 'force-dynamic';
+
 import connectDB from '@/config/database';
 import Message from '@/models/Message';
 import { getSessionUser } from '@/utils/getSessionUser';
-
-export const dynamic = 'force-dynamic';
 
 // GET /api/messages/unread-count
 export const GET = async (request) => {

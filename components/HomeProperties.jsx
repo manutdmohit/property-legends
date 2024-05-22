b4b61fe8f3b44759915913dc79f20b30c97a3c46
Mohit Saud <mohit@prabidhilabs.com>
2024-05-22 13:26:52 +0545

    Undo: Set recentProperties to empty array

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -4,12 +4,11 @@ import PropertyCard from './PropertyCard';
 import { fetchProperties } from '@/utils/requests';
 
 const HomeProperties = async () => {
-  // const data = await fetchProperties();
+  const data = await fetchProperties();
 
-  // const recentProperties = data.properties
-  //   .sort(() => Math.random() - Math.random())
-  //   .slice(0, 3);
-  const recentProperties = [];
+  const recentProperties = data.properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 3);
 
   return (
     <>
